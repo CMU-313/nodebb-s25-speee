b7aebcfd41654ec84f0efed4ef810e54aa9b8ebd
Elizabetht1 <eterveen@andrew.cmu.edu>
2025-02-19 23:18:22 -0500

    initial commit; started setting up client side scripts to handle backend things

---
+++ nodebb-plugin-endorsements/public/lib/handleButton.js
@@ -0,0 +1,8 @@
+
+define('/post/buttons/endorse',[],function(){
+    /** need to potentially update user tag so that it works */
+    $("#button").on("click",function(){
+        console.log("button was clicked!\n");
+    });
+
+})
